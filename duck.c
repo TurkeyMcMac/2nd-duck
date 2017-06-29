6db90d3c753fbcc9829f903da50497f9e1281b11
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-06-29 15:42:43 -0400

    Add options.

--- duck.c
+++ duck.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "settings.h"
 
@@ -59,12 +60,43 @@ char *randomPhrase(char *randomize) {
 }
 
 int main(int argc, char *argv[]) {
-	if (!strcmp(argv[1], "echo")) {
-		sayFromText(argv[2], argv[3]);
-	} else if (!strcmp(argv[1], "rand")) {
-		char phrase[MAX_READ_PHRASE_LEN];
-		memset(phrase, 0, MAX_READ_PHRASE_LEN);
-		sayFromText(argv[2], randomPhrase(phrase));
+	char opt,
+	     optMaybe;
+	bool hasOpt = false;
+	while ((optMaybe = getopt(argc, argv, "ehr")) != -1) {
+		if (hasOpt) {
+			puts("Multiple options are unnecessary.");
+			return -1;
+		}
+		opt = optMaybe;
+		hasOpt = true;
+	}
+	switch (opt) {
+		case 'e': {
+			int waitIndex;
+			int messageIndex;
+			const char *optArg = "-e";
+			if (!strcmp(argv[1], optArg)) {
+				waitIndex = 2;
+				messageIndex = 3;
+			} else if (!strcmp(argv[2], optArg)) {
+				waitIndex = 1;
+				messageIndex = 3;
+			} else {
+				waitIndex = 1;
+				messageIndex = 2;
+			}
+			sayFromText(argv[waitIndex], argv[messageIndex]);
+			break; }
+		case 'r': {
+			int waitIndex = !strcmp(argv[1], "-r") ? 2 : 1;
+			char phrase[MAX_READ_PHRASE_LEN];
+			memset(phrase, 0, MAX_READ_PHRASE_LEN);
+			sayFromText(argv[waitIndex], randomPhrase(phrase)); }
+			break;
+		default:
+			puts("Placeholder help information.");
+			break;
 	}
 	return 0;
 }
