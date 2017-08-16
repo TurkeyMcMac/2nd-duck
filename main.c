dd73ee6bea0af779bb09d73a5a4370dbe9167ce6
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-08-15 22:13:39 -0400

    Add more exit status stuff

--- main.c
+++ main.c
@@ -10,30 +10,42 @@
 #include "speakage.h"
 
 int main(int argc, char *argv[]) {
+	int exitStatus = 0;
 	char opt,
 	     optMaybe;
 	bool hasOpt = false;
 	while ((optMaybe = getopt(argc, argv, "ehpr")) != -1) {
 		if (hasOpt) {
 			puts("Multiple options are unnecessary.");
-			return -1;
+			exitStatus = -3;
+			goto help;
 		}
 		opt = optMaybe;
 		hasOpt = true;
 	}
 	switch (opt) {
 		case 'e':
-			if (argc < 4) goto help;
+			if (argc < 4) {
+				exitStatus = -1;
+				goto help;
+			}
 			sayFromText(argv[2], argv[3]);
 			break;
+		case 'h':
+			goto help;
 		case 'p':
 			puts(PHRASES_LOCATION);
 			break;
 		case 'r':
-			if (argc < 3) goto help;
+			if (argc < 3) {
+				exitStatus = -2;
+				goto help;
+			}
 			sayFromText(argv[2], randomPhrase());
 			break;
-		default: help:
+		default: 
+			exitStatus = -4;
+		help:
 			puts(
 "Usage: duck [option] [wait] [phrase]\n\
   options:\n\
@@ -46,5 +58,5 @@ int main(int argc, char *argv[]) {
 		    character.");
 			break;
 	}
-	return 0;
+	return exitStatus;
 }
