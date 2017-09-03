e055bf131be8e14903eb1583a42aaf63e86aac93
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-09-03 12:14:22 -0400

    Make pointer notation consistent for file-line-reader

--- file-line-reader.c
+++ file-line-reader.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t rflarr (char *arr[], FILE *file, const int maxLines, const int maxLength) {
+size_t rflarr (char **arr, FILE *file, const int maxLines, const int maxLength) {
 	for (size_t i = 0; i < maxLines; ++i) {
 		arr[i] = malloc(maxLength);
 		if (!fgets(arr[i], maxLength, file)) return i;
