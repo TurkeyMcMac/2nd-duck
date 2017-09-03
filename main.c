19ced951033f94928d258e950e4d7df127eff7a0
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-09-03 11:57:28 -0400

    Remove unnecessary use of goto

--- main.c
+++ main.c
@@ -10,46 +10,8 @@
 #include "settings.h"
 #include "speakage.h"
 
-int main(int argc, char *argv[]) {
-	int exitStatus = 0;
-	char opt,
-	     optMaybe;
-	bool hasOpt = false;
-	while ((optMaybe = getopt(argc, argv, "ehpr")) != -1) {
-		if (hasOpt) {
-			puts("Multiple options are unnecessary.");
-			exitStatus = -3;
-			goto help;
-		}
-		opt = optMaybe;
-		hasOpt = true;
-	}
-	switch (opt) {
-		case 'e':
-			if (argc < 4) {
-				exitStatus = -1;
-				goto help;
-			}
-			sayFromText(argv[2], argv[3]);
-			break;
-		case 'h':
-			goto help;
-		case 'p':
-			puts(PHRASES_LOCATION);
-			break;
-		case 'r':
-			if (argc < 3) {
-				exitStatus = -2;
-				goto help;
-			}
-			srand(time(NULL));
-			sayFromText(argv[2], randomPhrase());
-			break;
-		default: 
-			exitStatus = -4;
-		help:
-			puts(
-"Usage: duck [option] [wait] [phrase]\n\
+const char *HELP = "\
+Usage: duck [option] [wait] [phrase]\n\
   options:\n\
     -e wait phrase  Echo the specified phrase, waiting the specified\n\
                     time between saying each character.\n\
@@ -57,7 +19,49 @@ int main(int argc, char *argv[]) {
     -p              Print the path of the file containing phrases.\n\
     -r wait         Say a phrase picked randomly from the phrase file,\n\
                     waiting the specified time between saying each\n\
-		    character.");
+		    character.";
+
+int main(int argc, char *argv[]) {
+	char opt,
+	     optMaybe;
+	bool hasOpt = false;
+	while ((optMaybe = getopt(argc, argv, "ehpr")) != -1) {
+		if (hasOpt) {
+			puts("Multiple options are unnecessary.");
+			puts(HELP);
+			return -3;
+		}
+		opt = optMaybe;
+		hasOpt = true;
+	}
+	int exitStatus = 0;
+	switch (opt) {
+		case 'e':
+			if (argc < 4) {
+				exitStatus = -1;
+				puts(HELP);
+			} else {
+				sayFromText(argv[2], argv[3]);
+			}
+			break;
+		case 'h':
+			puts(HELP);
+			break;
+		case 'p':
+			puts(PHRASES_LOCATION);
+			break;
+		case 'r':
+			if (argc < 3) {
+				exitStatus = -2;
+				puts(HELP);
+			} else {
+				srand(time(NULL));
+				sayFromText(argv[2], randomPhrase());
+			}
+			break;
+		default: 
+			exitStatus = -4;
+			puts(HELP);
 			break;
 	}
 	return exitStatus;
